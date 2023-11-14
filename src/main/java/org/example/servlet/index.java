c8a78dea23a29a87fb380a52b2f489e402d22459
NathanKS <nathanks0210@gmail.com>
2023-11-14 16:07:07 -0300

    feat: servlet index

---
+++ src/main/java/org/example/servlet/index.java
@@ -0,0 +1,18 @@
+package org.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class index extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/plain");
+        response.getWriter().println("Servidor rodando!");
+
+    }
+
+}
