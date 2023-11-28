e14f3a7b11fb60421e33f2fbbee35e78bd1c4c53
NathanKS <nathanks0210@gmail.com>
2023-11-28 15:01:14 -0300

    feat: update produto DTO

---
+++ src/main/java/org/example/presentation/DTO/UpdateProdutoDTO.java
@@ -0,0 +1,31 @@
+package org.example.presentation.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateProdutoDTO {
+
+    private String descricao;
+
+    private Double preco;
+
+    private Integer quantidade;
+
+    private Integer estoque_min;
+
+    private Boolean lativo;
+
+    public UpdateProdutoDTO() {
+    }
+
+    public UpdateProdutoDTO(String descricao, Double preco, Integer quantidade, Integer estoque_min, Boolean lativo) {
+        this.descricao = descricao;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.estoque_min = estoque_min;
+        this.lativo = lativo;
+    }
+
+}
