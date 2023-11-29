5f45b3b6aac58a992f0f760a63cff814f09b4d76
NathanKS <nathanks0210@gmail.com>
2023-11-29 10:18:08 -0300

    feat: get produto service

---
+++ src/main/java/org/example/domain/service/GetProdutoService.java
@@ -0,0 +1,49 @@
+package org.example.domain.service;
+
+import org.example.data.Produto;
+import org.example.data.repository.ProdutoRepository;
+import org.example.util.LocalizadorDeServico;
+import org.example.util.exception.NotFoundException;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public abstract class GetProdutoService {
+
+    public static Produto execute(String parametro) throws NotFoundException {
+
+        ProdutoRepository produtoRepository = LocalizadorDeServico.getProdutoRepository();
+
+        Optional<Produto> produto = Optional.empty();
+
+        try {
+            produto = produtoRepository.findByHash(UUID.fromString(parametro));
+
+            if (produto.isPresent()) {
+                return produto.get();
+            }
+        }catch (IllegalArgumentException e) {
+            // do nothing
+        }
+
+        produto = produtoRepository.findByEan13(parametro);
+
+        if (produto.isPresent()) {
+            return produto.get();
+        }
+
+        produto = produtoRepository.findByNome(parametro);
+
+        if (produto.isPresent()) {
+            return produto.get();
+        }
+
+        produto = produtoRepository.findById(Long.valueOf(parametro));
+
+        if (produto.isPresent()) {
+            return produto.get();
+        }
+
+        throw new NotFoundException("Produto não encontrado", 404);
+    }
+}
