123f1ef47c37931f8593366188f76e60aba92fdd
NathanKS <nathanks0210@gmail.com>
2023-11-29 14:39:20 -0300

    feat: invalidParams Exception

---
+++ src/main/java/org/example/util/exception/InvalidParamsExcption.java
@@ -0,0 +1,8 @@
+package org.example.util.exception;
+
+public class InvalidParamsExcption extends ProductExcption {
+
+    public InvalidParamsExcption(String message, Integer status) {
+        super(message, status);
+    }
+}
