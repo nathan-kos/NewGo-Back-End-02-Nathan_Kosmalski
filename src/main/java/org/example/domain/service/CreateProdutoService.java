5ec7724173add4277662a929e240a1f4de5e20ab
NathanKS <nathanks0210@gmail.com>
2023-12-05 15:39:32 -0300

    fix: não permitir criar produto com valores negativos

--- src/main/java/org/example/domain/service/CreateProdutoService.java
+++ src/main/java/org/example/domain/service/CreateProdutoService.java
@@ -5,13 +5,14 @@ import org.example.data.repository.ProdutoRepository;
 import org.example.util.LocalizadorDeServico;
 import org.example.util.exception.ExistingProductException;
 import org.example.util.exception.InternalServerErrorException;
+import org.example.util.exception.InvalidParamsException;
 import org.example.util.exception.InvalidProductException;
 
 import java.util.Optional;
 
 public abstract class CreateProdutoService {
 
-    public static Produto execute(Produto produto) throws InternalServerErrorException, ExistingProductException, InvalidProductException {
+    public static Produto execute(Produto produto) throws InternalServerErrorException, ExistingProductException, InvalidProductException, InvalidParamsException {
 
        ProdutoRepository repository = LocalizadorDeServico.getProdutoRepository();
 
@@ -19,16 +20,28 @@ public abstract class CreateProdutoService {
            throw new InvalidProductException("Nome do produto inválido", 400);
        }
 
-       if(produto.getEstoque_min() == null || produto.getEstoque_min() < 0){
+       if(produto.getEstoque_min() == null){
            produto.setEstoque_min(0);
        }
 
-       if(produto.getQuantidade() == null || produto.getQuantidade() < 0){
+       if(produto.getEstoque_min() < 0){
+           throw new InvalidParamsException("Estoque minimo inválido", 400);
+       }
+
+       if(produto.getQuantidade() == null){
            produto.setQuantidade(0);
        }
 
-       if(produto.getPreco() == null || produto.getPreco() < 0){
+       if(produto.getQuantidade() < 0){
+           throw new InvalidParamsException("Quantidade inválida", 400);
+       }
+
+       if(produto.getPreco() == null){
            produto.setPreco(0.0);
+       }
+
+       if(produto.getPreco() < 0){
+           throw new InvalidParamsException("Preço inválido", 400);
        }
 
        if(produto.getEan13() == null) {
