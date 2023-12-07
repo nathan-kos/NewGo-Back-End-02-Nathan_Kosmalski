37daca7b491fcc20f6d07429114235c929625f4a
NathanKS <nathanks0210@gmail.com>
2023-12-07 12:23:46 -0300

    fix: formato da data retornado

--- src/main/java/org/example/util/JsonConverter.java
+++ src/main/java/org/example/util/JsonConverter.java
@@ -1,15 +1,24 @@
 package org.example.util;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.time.LocalDateTime;
 
 public abstract class JsonConverter {
 
+    private static final Gson gson = new GsonBuilder()
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+            .setPrettyPrinting()
+            .create();
+
+
     public static String toJson(Object object) {
-        return new Gson().toJson(object);
+        return gson.toJson(object);
     }
 
     public static <T> T fromJson(String json, Class<T> classOfT) {
-        return new Gson().fromJson(json, classOfT);
+        return gson.fromJson(json, classOfT);
     }
 
 }
